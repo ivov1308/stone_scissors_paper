765bd1c19ca5bcf3f5d66bfc531db4aab5544e43
ivov1308 <ivan13air@mail.ru>
2026-04-02 12:06:25 +0300

    v.2026

--- config/config.py
+++ config/config.py
@@ -8,15 +8,24 @@ class TgBot:
 
 
 @dataclass
+class LogSettings:
+    level: str
+    format: str
+
+
+@dataclass
 class Config:
-    tg_bot: TgBot
+    bot: TgBot
+    log: LogSettings
 
 
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
     return Config(
-        tg_bot=TgBot(
-            token=env('BOT_TOKEN')
+        bot=TgBot(token=env('BOT_TOKEN')),
+        log=LogSettings(
+            level=env('LOG_LEVEL'),
+            format=env('LOG_FORMAT')
         )
     )
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,44 +1,33 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from lexicon.lexicon import LEXICON_RU
 
+# ------- Создаём клавиатуру через ReplyKeyboardBuilder -------
 
-# kb_start
-_buttons_start = [[
-    KeyboardButton(text='Начать игру'),
-    KeyboardButton(text='Правила игры'),
-    KeyboardButton(text='Счёт'),
-]]
-keyboard_start = ReplyKeyboardMarkup(
-    keyboard=_buttons_start,
-    resize_keyboard=True
+# Создаём кнопки с ответами согласия и отказа
+button_yes = KeyboardButton(text=LEXICON_RU["yes_button"])
+button_no = KeyboardButton(text=LEXICON_RU["no_button"])
+
+# Инициализируем билдер для клавиатуры с кнопками "✅ Давай" и "Не хочу!"
+yes_no_kb_builder = ReplyKeyboardBuilder()
+
+# Добавляем кнопки в билдер с аргументом width=2
+yes_no_kb_builder.row(button_yes, button_no, width=2)
+
+# Создаём клавиатуру с кнопками "✅ Давай!" и "Не хочу!"
+yes_no_kb: ReplyKeyboardMarkup = yes_no_kb_builder.as_markup(
+    one_time_keyboard=True, resize_keyboard=True
 )
 
-# kb_game
-_buttons_game = [[
-    KeyboardButton(text='Камень ✊🏼'),
-    KeyboardButton(text='Ножницы ✌🏼'),
-    KeyboardButton(text='Бумага ✋🏼')
-]]
-keyboard_game = ReplyKeyboardMarkup(
-    keyboard=_buttons_game,
-    resize_keyboard=True
-)
+# ------- Создаём игровую клавиатуру без использования билдера -------
 
-# kb_continue
-_buttons_continue = [[
-    KeyboardButton(text='Сыграть ещё'),
-    KeyboardButton(text='Выйти в меню')
-]]
-keyboard_continue = ReplyKeyboardMarkup(
-    keyboard=_buttons_continue,
-    resize_keyboard=True
-)
+# Создаём кнопки игровой клавиатуры
+button_1 = KeyboardButton(text=LEXICON_RU["rock"])
+button_2 = KeyboardButton(text=LEXICON_RU["scissors"])
+button_3 = KeyboardButton(text=LEXICON_RU["paper"])
 
-# kb_score
-_buttons_score = [[
-    KeyboardButton(text='Сбросить счёт'),
-    KeyboardButton(text='Выйти в меню')
-]]
-keyboard_score = ReplyKeyboardMarkup(
-    keyboard=_buttons_score,
-    resize_keyboard=True
+# Создаём игровую клавиатуру с кнопками "Камень 🗿",
+# "Ножницы ✂️" и "Бумага 📜" как список списков
+game_kb = ReplyKeyboardMarkup(
+    keyboard=[[button_1], [button_2], [button_3]], resize_keyboard=True
 )
